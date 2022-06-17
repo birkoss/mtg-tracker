e58419bbeaa11145931005e7ffcdc43cef31d86f
Mathieu Robichaud <admin@birkoss.com>
2022-06-17 07:55:42 -0400

    Hold to increase/decrease amounts

--- lib/widgets/ui/amount/button.dart
+++ lib/widgets/ui/amount/button.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class AmountButton extends StatelessWidget {
+class AmountButton extends StatefulWidget {
   final String label;
   final VoidCallback onPress;
 
@@ -11,21 +13,38 @@ class AmountButton extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<AmountButton> createState() => _AmountButtonState();
+}
+
+class _AmountButtonState extends State<AmountButton> {
+  Timer? _timer;
+
+  @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
-      child: InkWell(
-        onTap: onPress,
-        child: Container(
-          alignment: Alignment.center,
-          width: MediaQuery.of(context).size.width * 0.25,
-          height: double.infinity,
-          child: Text(
-            label,
-            style: Theme.of(context).textTheme.headline1!.copyWith(
-                  color: Colors.white30,
-                  fontSize: 40,
-                ),
+      child: GestureDetector(
+        onPanCancel: () {
+          _timer?.cancel();
+        },
+        onPanDown: (_) {
+          _timer = Timer.periodic(const Duration(milliseconds: 150), (_) {
+            widget.onPress();
+          });
+        },
+        child: InkWell(
+          onTap: widget.onPress,
+          child: Container(
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width * 0.25,
+            height: double.infinity,
+            child: Text(
+              widget.label,
+              style: Theme.of(context).textTheme.headline1!.copyWith(
+                    color: Colors.white30,
+                    fontSize: 40,
+                  ),
+            ),
           ),
         ),
       ),
