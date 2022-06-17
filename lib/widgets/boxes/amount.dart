f9597e3b4b112e13163b07e3997169dacb16243c
Mathieu Robichaud <admin@birkoss.com>
2022-06-17 07:40:57 -0400

    UI Tweaks

--- lib/widgets/boxes/amount.dart
+++ lib/widgets/boxes/amount.dart
@@ -76,35 +76,38 @@ class _AmountBox extends State<AmountBox> {
         ),
         Expanded(
           flex: 1,
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: widget.onPress,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  AmountText(
-                    isVisible: _isTooltipVisible,
-                    onHidden: () {
-                      setState(() {
-                        _amountChanges = 0;
-                      });
-                    },
-                    amount: _amountChanges,
-                  ),
-                  Text(
-                    widget.getValue(),
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.headline1,
-                  ),
-                  widget.child != null
-                      ? widget.child!
-                      : const SizedBox(
-                          height: 2,
-                        ),
-                ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AmountText(
+                isVisible: _isTooltipVisible,
+                onHidden: () {
+                  setState(() {
+                    _amountChanges = 0;
+                  });
+                },
+                amount: _amountChanges,
               ),
-            ),
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: widget.onPress,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      widget.getValue(),
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
+                  ),
+                ),
+              ),
+              widget.child != null
+                  ? widget.child!
+                  : const SizedBox(
+                      height: 2,
+                    ),
+            ],
           ),
         ),
         Expanded(
